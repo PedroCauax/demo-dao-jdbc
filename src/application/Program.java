6099eece4c07918590376187f90f4aaae03317db
Pedro Cauã <pcaua6372@gmail.com>
2026-01-20 13:46:53 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +13,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		 Scanner sc = new Scanner(System.in);
+		 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("=== Test 1: seller findById ====");
@@ -43,6 +45,14 @@ public class Program {
 		seller.setName("Marta Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed!");
+		
+		System.out.println("===\n Test 5: seller delete ====");
+		System.out.println("Enter id for delete test");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 
 }
